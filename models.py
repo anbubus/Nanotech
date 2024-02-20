84eab1db44ab2057fa36c8d9c91caeac8567daec
Fernanda-a <myfernam@gmail.com>
2024-02-19 22:56:20 -0300

    Creating login validation and more

--- models.py
+++ models.py
@@ -1,10 +1,22 @@
 from browser import document
 from browser.widgets.dialog import InfoDialog
+from db_connection import get_database as db
 
 def checkpass(n,p):
     if not p:
         InfoDialog("Alert", f"Senha Vazia")
     if not n:
-        InfoDialog("Alert", f"Nome Vazio")
+        InfoDialog("Alert", f"Email Vazio")
 
     return True
+
+#ela ta recebendo n de email, p de senha e d da tabela users 
+def verify(n,p,d):
+    db = db
+    users = db[d]
+    for user in users:
+        email = user['email']
+        password = user['password']
+        if n == email and p == password:
+
+            return True
--- server.py
+++ server.py
@@ -1,13 +1,19 @@
 from browser import document
 from browser.widgets.dialog import InfoDialog
-from models import checkpass
+from models import checkpass, verify
+from db_connection import get_database as db
 
 def click(ev):
+    email = document['email'].value
+    password = document['password'].value
 
-    name = document['name'].value
-    password = document['pass'].value
 
+    if checkpass(email, password):
+        if not verify(email, password, "users"):
+            InfoDialog("Alert", f"Email ou senha incorretos.")
 
-    if checkpass(name, password):
-        InfoDialog("Alert", f"Login executado com sucesso, {name}!!!")
+        if verify(email, password, "users"):
+            InfoDialog("Alert", f"Login executado com sucesso, {email}!!")
+
 document['bt_'].bind("click", click)
+
